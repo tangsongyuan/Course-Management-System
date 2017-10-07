bba99e326d644c41b2729a5fee0c25267b523776
tangsongyuan <songyuantang@outlook.com>
2017-10-07 04:46:07 +0000

    add update

--- index.php
+++ index.php
@@ -33,6 +33,7 @@
 		
 		<form Method = "Post" Action = "select.php">
 			SELECTION
+			<br>
 			To review personal information, please enter ID (1-500):
 			<input type="text" name="id1">
 			<input type="Submit" value="Submit">
@@ -55,6 +56,69 @@
 	<hr>
 	
 	
-
+	UPDATE
+	<?php
+		$temp = "";
+		if ($_SERVER["REQUEST_METHOD"] == "POST") {
+			if (empty($_POST["name"])) {
+				$nameEr = "Name is required";
+			} else {
+				$temp = $_POST["name"];
+				if (!preg_match('/^[0-9]*$/', $temp)) {
+					echo "Only number from 1 to 500";
+				}
+			}
+		} 
+	?>
+	
+	<form method="Post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+		Your old information:
+		ID:
+		<input type="text" name="name" value="<?php echo $temp; ?>">
+		<input type="Submit" name="Submit">
+	</form>
+    <?php
+        if ($temp == "") {
+            // empty input
+        } else {
+            $query = "SELECT * FROM students WHERE id = $temp";
+            $result = mysqli_query($connection, $query);
+            while ($row = mysqli_fetch_assoc($result)) {
+    ?>
+        <table>
+            <thead>
+                <td>ID</td>
+				<td>Name</td>
+				<td>Gender</td>
+				<td>Age</td>
+				<td>Email</td>
+            </thead>
+            <tr>
+                <td><?php echo $row["id"] ?></td>
+                <td><?php echo $row["name"] ?></td>
+                <td><?php echo $row["gender"] ?></td>
+                <td><?php echo $row["age"] ?></td>
+                <td><?php echo $row["email"] ?></td>
+            </tr>
+        </table>
+    <?php
+            }
+        }
+    ?>
+    <form method="Post" action="update.php">
+    	Please update Information.
+    	ID:
+    	<input type="text" name="id">
+    	Name:
+    	<input type="text" name="name">
+    	Gender:
+    	<input type="text" name="gender">
+    	Age:
+    	<input type="text" name="age">
+    	Email:
+    	<input type="text" name="email">
+    	<input type="Submit" name="Update">
+    </form>
+    <hr>
 	</body>
 </html>
---
+++ update.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Update</title>
+</head>
+<body>
+
+	<?php
+	    
+	    //Connect to the database
+	    $host = "127.0.0.1";
+	    $user = "tangsongyuan";                     //Your Cloud 9 username
+	    $pass = "";                                  //Remember, there is NO password by default!
+	    $db = "c9";                                  //Your database name you want to connect to
+	    $port = 3306;                                //The port #. It is always 3306
+	    
+	    $connection = mysqli_connect($host, $user, $pass, $db, $port)or die(mysql_error());
+	
+	?>
+	<a href="index.php">Go Back</a>
+	
+	<?php
+	    $id = $_POST["id"];
+	    $name = $_POST["name"];
+	    $gender = $_POST["gender"];
+	    $age = $_POST["age"];
+	    $email = $_POST["email"];
+	    
+	    $query = "UPDATE students SET  name='{$name}', gender='{$gender}', age='{$age}', email='{$email}' WHERE id=$id";
+	    $result = mysqli_query($connection, $query);
+	    if ($result) {
+	        echo "Update Successfully.";
+	    } else {
+	        echo "Error in updating:" . $connection->error;
+	    }
+	?>
+	
+	Your updated information:
+	<?php
+	    $query = "SELECT * FROM students WHERE id = $id";
+	    $result = mysqli_query($connection, $query);
+	    while ($row = mysqli_fetch_assoc($result)) {
+	?>
+	        <table>
+        		<thead>
+        			<tr>
+        				<td>ID</td>
+        				<td>Name</td>
+        				<td>Gender</td>
+        				<td>Age</td>
+        				<td>Email</td>
+        			</tr>
+        		</thead>
+        		<tr>
+					<td><?php echo $row["id"]?></td>
+					<td><?php echo $row["name"]?></td>
+					<td><?php echo $row["gender"]?></td>
+					<td><?php echo $row["age"]?></td>
+					<td><?php echo $row["email"]?></td>
+				</tr>
+			</table>
+    <?php
+	    }
+	?>
+	
+	
+	<!--$query = "SELECT * FROM students WHERE id = $id1";-->
+ <!--   $result = mysqli_query($connection, $query);-->
+ <!--   while ($row = mysqli_fetch_assoc($result)) {-->
+	
+</body>
+</html>
